62b1c9175e64b84cebb74a6dcf1a7fbdd61c5296
张懿麒 <zhangyiqi55732@sina.com>
2017-06-29 23:30:44 +0800

    fst

--- ConsoleApplication9/ConsoleApplication9.cpp
+++ ConsoleApplication9/ConsoleApplication9.cpp
@@ -2,10 +2,59 @@
 //
 
 #include "stdafx.h"
-
+# include <iostream>
+using namespace std;
 
 int main()
 {
-    return 0;
+	int m, n;
+	cin >> n;
+	cin >> m;
+	bool * facing = new bool[n];
+	char * title = new char[n];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> facing[i];
+		cin >> title[i];
+	}
+	bool * direction = new bool[m];
+	int * steps = new int[m];
+	for (int i = 0; i < m; i++)
+	{
+		cin >> direction[i];
+		cin >> steps[i];
+	}
+	int result = 0;
+	for (int i = 0; i < m; i++)
+	{
+		int dir = direction[i];
+		int fac = facing[result];
+		int stp = steps[i];
+		if (facing)
+		{
+			if (dir = 1) { result += stp; }
+			else { result -= stp; }
+		}
+		else
+		{
+			if (dir = 1) { result -= stp; }
+			else { result += stp; }
+		}
+		bool stat = 1;
+		while (stat)
+		{
+			if (result >= n) {
+				result -= n;
+			}
+			if (result < 0) {
+				result += n;
+			}
+			if (result >= 0 && result < 7)
+			{
+				stat = false;
+			}
+		}
+	}
+	cout << title[result];
+	return 0;
 }
-
